5ff107b322f3d560e1329c59f8ac2ed56ece110a
master_jiho <aksjajsxm123@gmail.com>
2022-04-24 20:25:06 +0900

    미들웨어 이름 통합

--- src/router/router.js
+++ src/router/router.js
@@ -2,39 +2,43 @@ const express = require("express");
 const router = express.Router();
 
 
-router.get("/", "로그인 미들웨어",
+router.get("/", "보안 미들웨어",
 (req,res) =>{    
     res.render("인덱스페이지")      //html 파일은 어디에..?
 });
 
-router.get("/login",(req, res)=>{
+router.get("/login",
+    (req, res)=>{
     res.render("로그인 페이지");
 })
-router.post("/login", (req, res)=>{
+router.post("/login", 
+    (req, res)=>{
     res.render("home")
 })
 
-router.get("/home", (res, req) =>{
+router.get("/home", "보안 미들웨어",
+    (res, req) =>{
     res.render("home");
 })
 
 
-router.post("/input", "보안 미들웨어",(res,req)=>{
+router.post("/input", 
+    "보안 미들웨어",(res,req)=>{
     res.render("/detail");
 })
 
-router.get("/detail", "미들웨어",(res,req)=>{
+router.get("/detail", "보안 미들웨어",(res,req)=>{
     res.render("/detail page");
 })
 
-router.post("/detail", "미들웨어", (res,req)=>{
+router.post("/detail", "보안 미들웨어", (res,req)=>{
     res.render("/detail")
 })
 
-router.get("/profile", "미들웨어", (res, req)=>{
+router.get("/profile", "보안 미들웨어", (res, req)=>{
     res.render("/profile")
 })
 
-router.post("/profile", "미들웨어", (res, req)=>{
+router.post("/profile", "보안 미들웨어", (res, req)=>{
     res.render("/profile")
 })
