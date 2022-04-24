444863691da76450242ca8138f60178bffc404d7
master_jiho <aksjajsxm123@gmail.com>
2022-04-24 19:34:10 +0900

    업데이트

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const react = require("react");
 
 app.listen(process.env.PORT, ()=>{
     console.log("서버가 가동되었습니다");
@@ -9,4 +10,14 @@ app.listen(process.env.PORT, ()=>{
 
 
 mongoose.connect("mongodb://localhost:27017/[디비 이름]",
-{useNewUrlParser: true, useInifiedTopology: true});
+    {useNewUrlParser: true, useInifiedTopology: true});
+
+mongoose.connection.on('error', console.error.bind(console, "에러가 발생했습니다"));
+
+
+app.get('/', (req, res)=>{
+    
+});
+
+
+// Proxy, axios 공부해오기
